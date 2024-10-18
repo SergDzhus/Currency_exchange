f004f01a4c4d2f1e6a1fc7d99e1e536fca41f365
SergDzhus <SDzhoos@gmail.com>
2024-10-18 11:53:24 +0300

    Ф-ция ввода кода валюты и обработка ошибок..

--- Currency_exch.py
+++ Currency_exch.py
@@ -4,14 +4,26 @@ import json
 import pprint
 from  tkinter import messagebox as mb
 
+from bottle import response
+
 
 def exchange():
+    code = entry.get()
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за один доллар")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка: {e}!")
+    esle:
+        mb.showwarning("Внимание!", "Введите код валюты!")
 
-result = requests.get('https://open.er-api.com/v6/latest/USD')
-data = json.loads(result.text)
-p = pprint.PrettyPrinter(indent=4)
-
-p.pprint(data)
 
 window = Tk()
 window.title("Курсы обмена валют")
