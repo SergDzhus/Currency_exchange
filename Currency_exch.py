8779b193d151a4b5824d6570445bf59abd79ea98
SergDzhus <SDzhoos@gmail.com>
2024-10-18 11:31:45 +0300

    Импортировали pprint и сделали вывод через него.

--- Currency_exch.py
+++ Currency_exch.py
@@ -1,1 +1,10 @@
-from tkinter import *
+# from tkinter import *
+import requests
+import json
+import pprint
+
+result = requests.get('https://open.er-api.com/v6/latest/USD')
+data = json.loads(result.text)
+p = pprint.PrettyPrinter(indent=4)
+
+p.pprint(data)
